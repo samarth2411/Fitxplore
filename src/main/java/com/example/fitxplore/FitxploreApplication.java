c7f656031d34aef856b5be3d32d66d8b84fdb123
samarth2411 <73635998+samarth2411@users.noreply.github.com>
2022-11-10 17:17:42 +0530

    initial commit

---
+++ src/main/java/com/example/fitxplore/FitxploreApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitxplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitxploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitxploreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fitxplore/FitxploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitxplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitxploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
